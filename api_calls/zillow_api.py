dc71868e921eab47fa64ef3ccf6d56e779dbd43a
Ricky Huget <117780303+rickyhuget@users.noreply.github.com>
2024-02-05 23:28:39 -0800

    Update zillow_api.py

--- api_calls/zillow_api.py
+++ api_calls/zillow_api.py
@@ -38,26 +38,3 @@ def zillowApi_getAddressesForSale(city, state):
         file_path = folder_properties + city + "_" + state + "_properties.json"
         with open(file_path, 'w') as json_file:
             json.dump(propertyDict, json_file, indent=4)
-
-
-def zillowApi_getZoningData(zpid):
-    url = "https://zillow-com1.p.rapidapi.com/property"
-    querystring = {"zpid":zpid}
-    headers = {
-        "X-RapidAPI-Key": returnZillowKey(),
-        "X-RapidAPI-Host": "zillow-com1.p.rapidapi.com"
-    }
-    try:
-        response = requests.get(url, headers=headers, params=querystring)
-        status_code = response.status_code
-        if status_code == 200:
-            results = response.json()
-            resoFacts = results['resoFacts']
-            zoning = resoFacts['zoning']
-            zoningDescription = resoFacts['zoningDescription']
-            parcelNumber = resoFacts['parcelNumber']
-            return [str(parcelNumber), str(zoning), str(zoningDescription)]
-        else:
-            return {"error": "Failed to fetch data"}
-    except requests.RequestException as e:
-        return {"error": f"RequestException: {e}"}
